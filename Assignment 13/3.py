1d6267f638356bf1b94e2c599f39a7a49a377eff
Malik Zeeshan <malikzeeshan9601@gmail.com>
2024-11-11 12:07:27 +0500

    3.py of Assignment 13 has been added

---
+++ Assignment 13/3.py
@@ -0,0 +1,10 @@
+# 3. Write a function `calculate_average` that takes a list of grades and returns the average grade.
+# Example:
+# Input: calculate_average([90, 80, 70])
+# Output: 80.0
+def calculate_average(grades):
+    total_marks = sum(grades)
+    return total_marks/len(grades)
+average = calculate_average([90,80,70])
+print("The average is: " , average)
+print("\n")
