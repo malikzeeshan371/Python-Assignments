c8f73093d77d06869c789eabc28a3a9d6b398cf2
Malik Zeeshan <malikzeeshan9601@gmail.com>
2024-11-28 21:59:18 +0500

    added 6.py to Assignment 14

---
+++ Assignment 14/6.py
@@ -0,0 +1,18 @@
+
+
+# 6. Write a function `add_team_to_game` that takes a game dictionary and a team name with a list of players,
+# and adds the team to the game.
+# Example:
+# Input: game = {'game_name': 'Championship', 'teams': {'TeamA': ['Alice', 'Bob']}}
+#        game = {'game_name': 'Championship', 'teams': {'TeamA': ['Alice', 'Bob']}}
+# Output: {'game_name': 'Championship', 'teams': {'TeamA': ['Alice', 'Bob'], 'TeamB': ['Charlie', 'Dave']}}
+
+def add_team_to_game(game , team):
+    game.update(team)
+    return game
+
+game= {'game_name': 'Championship', 'teams': {'TeamA': ['Alice', 'Bob']}}
+# game= {'game_name': 'Championship', 'teams': {'TeamB': ['Alice', 'Bob']}}
+
+
+print(add_team_to_game(game,{'TeamB':['Charlie', 'Dave']}))
