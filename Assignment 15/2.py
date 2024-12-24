9fa97f6ff31ff9d6fd47bbcb82493522213e24d7
Malik Zeeshan <malikzeeshan9601@gmail.com>
2024-12-24 14:39:26 +0500

    added 2.py to Assignment 15

---
+++ Assignment 15/2.py
@@ -0,0 +1,14 @@
+
+# 2. Write a function `read_message_from_file` that takes a filename,
+# and returns the content of the file as a string.
+# Example:
+# Input: read_message_from_file('message.txt')
+# Output: 'Hello, World!'
+
+def read_message_from_file(filename):
+    with open(filename , 'r') as file:
+       contents =  file.read()
+    print(contents)
+
+Input = read_message_from_file('Assignment 15/message.txt')
+print(input)
