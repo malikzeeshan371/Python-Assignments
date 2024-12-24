255a100bfe975d50a1ae7e4efa0d8add695281af
Malik Zeeshan <malikzeeshan9601@gmail.com>
2024-12-24 14:40:25 +0500

    added 3.py to Assignment 15

---
+++ Assignment 15/3.py
@@ -0,0 +1,17 @@
+
+# 3. Write a function `append_message_to_file` that takes a filename and a string message,
+# and appends the message to the file.
+# Example:
+# Input: append_message_to_file('message.txt', ' Goodbye!')
+# (Content of 'message.txt' -> 'Hello, World! Goodbye!')
+
+def append_message_to_file(filename , message):
+    with open(filename , 'a') as file:
+        contents = file.write(message)
+
+    print ('this is to append the' , filename , 'with new message')
+
+
+
+Input =  append_message_to_file('Assignment 15/message.txt', ' Goodbye!')
+print(input)
