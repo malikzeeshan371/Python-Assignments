98f22e0a94c2cd1ada452e1d0a617ad5b1964bb2
Malik Zeeshan <malikzeeshan9601@gmail.com>
2024-12-29 23:54:28 +0500

    added 10.py after making changes

--- Assignment 15/10.py
+++ Assignment 15/10.py
@@ -1,3 +1,4 @@
+
 # 10. Write a function `file_statistics` that takes a filename,
 # and returns a dictionary with the number of characters, words, and lines in the file.
 # Example:
@@ -6,39 +7,51 @@
 # Output: {'characters': 49, 'words': 10, 'lines': 3}
 
 def file_statistics(filename):
-    number_of_characters = 0
-    number_of_words = 0
+    # number_of_characters = 0
+    # number_of_words = 0
 
-    with open(filename , 'r') as file:
+    # with open(filename , 'r') as file:
 
 
-    #    file = open(filename,'r')
+    # #    file = open(filename,'r')
     
+    # #  lines = len(file.readlines())
+    
+    #  data = file.read()
+    #  word_count = data.split()
+    #  number_of_words += len(word_count)
+
     #  lines = len(file.readlines())
-    
-     data = file.read()
-     word_count = data.split()
-     number_of_words += len(word_count)
 
-     lines = len(file.readlines())
-
-     for char in data:
-          char = number_of_characters
-          number_of_characters += char
-    print(char)
+    #  for char in data:
+    #       char = number_of_characters
+    #       number_of_characters += char
+    # print(char)
           
 
-    return{
-       'number of words' : number_of_words,
-       'no. of lines' : lines,
+    # return{
+    #    'number of words' : number_of_words,
+    #    'no. of lines' : lines
 
-    }
+    # }
+
+    file = open(filename , 'r')
+    lines = file.readlines()
+    file.close()
+    lines_count = len(lines)
+
+    word_count = 0
+    char_count = 0
+
+
+    for line in lines:
+        line_words = line.split()
+        word_count += len(line_words)
+
+        char_count += len(line)
+    
+    return {'characters': char_count, 'words': word_count, 'lines': lines_count}
+
 
        
-     
-
-
-
-input = file_statistics('Assignment 15/filestats.txt')
-print(input)
-   
+print(file_statistics('Assignment 15/filestats.txt'))
